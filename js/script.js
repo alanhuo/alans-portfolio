f8b77f9823f14ad6c30e054141c0ce4ac610ff87
Alan Huo <59664468+alanhuo@users.noreply.github.com>
2020-07-22 03:22:45 +0100

    commit summary
    lets commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function openMenu() {
+  document.getElementById("side-menu").classList.add("show-menu");
+}
+
+function closeMenu() {
+  document.getElementById("side-menu").classList.remove("show-menu");
+}
+
+document.getElementById("menu-trigger").addEventListener("click", openMenu);
+
+document.getElementById("menu-close").addEventListener("click", closeMenu);
+
+document.getElementById("trigger-close").addEventListener("click", closeMenu);
